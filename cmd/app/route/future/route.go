38683e7f0fc3ba9b7151ddfb28fe72dc58b5110c
Chanon Eiemrod <chanoneiemrod@Chanons-MacBook-Air.local>
2026-01-28 22:38:13 +0700

    add:bff  account management route

--- cmd/app/route/future/route.go
+++ cmd/app/route/future/route.go
@@ -1,12 +1,16 @@
 package route
 
 import (
+	accmanagementroute "tradething/app/bn/future/BFF/account_management/router"
 	accumroute "tradething/app/bn/future/accumulation/route"
 	marketdataroute "tradething/app/bn/future/market_data/route"
 	positionroute "tradething/app/bn/future/position/route"
 	positionhistoryroute "tradething/app/bn/future/position_history/route"
 	subaccountroute "tradething/app/bn/future/sub_account/route"
 	"tradething/config"
+
+	registeraccrepocoreservice "tradething/app/bn/future/sub_account/infrastructure/db/register_account"
+	subaccountcoreservice "tradething/app/bn/future/sub_account/service"
 
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/labstack/echo/v4"
@@ -17,6 +21,10 @@ func RouteFuture(
 	config *config.AppConfig,
 	dynamodbclient *dynamodb.Client,
 ) {
+	regsiterAccountRepositoryCoreServce := registeraccrepocoreservice.NewRegisterAccountRepository(dynamodbclient)
+	subaccountCoreService := subaccountcoreservice.NewSubAccountService(regsiterAccountRepositoryCoreServce)
+	accmanagementroute.Router(app_echo, subaccountCoreService)
+
 	if config.IsLocal() {
 		positionroute.Router(app_echo, dynamodbclient)
 		marketdataroute.Router(app_echo, config)
